dcebea1c0ea19f6f99afcb0cb591219c0ecd023c
apichaicoding <thinthonglang.apichai@gmail.com>
2024-03-29 21:09:22 +0700

    คำสั่งแชร์ลิ้งใหม่

--- commands/shar-live.js
+++ commands/shar-live.js
@@ -8,6 +8,8 @@ module.exports = {
     if (!message.author) return;
 
     const channel = message.guild.channels.cache.get(process.env.CHANNEL_FIND_FRIEND);
+    // const channel = message.guild.channels.cache.get(process.env.CHANNEL_TEST);
+    
     if (!channel) return console.error("Invalid channel ID");
 
     if (value2 != "") {
@@ -16,7 +18,7 @@ module.exports = {
       // สร้าง embed
       const exampleEmbed = new EmbedBuilder()
         .setColor(0xeb9b34)
-        .setTitle('มาไลฟ์กันเถอะ')
+        .setTitle("มาไลฟ์กันเถอะ กดดูไลฟ์ตรงนี้ได้เลย")
         .setURL(
           "https://www.tiktok.com/@valkillerchannel/live?enter_from_merge=share&enter_method=share_copy_link"
         )
@@ -26,18 +28,13 @@ module.exports = {
             "https://cdn.discordapp.com/attachments/1136706677748531240/1215354184422133880/LOGO_2023.png?ex=65fc71c9&is=65e9fcc9&hm=f28ee0c0f617f4805cc55759c7bc182135d714f55c6465fd26e852edddb18a10&",
           url: "https://www.tiktok.com/@valkillerchannel?is_from_webapp=1&sender_device=pc",
         })
-        .setDescription("Valkiller Channel is now live on Tiktok!")
-        .addFields({ name: "Description", value: "เริ่ม 20:30 - 23:00" })
-        .setThumbnail(
-          "https://cdn.discordapp.com/attachments/1136706677748531240/1215354184422133880/LOGO_2023.png?ex=65fc71c9&is=65e9fcc9&hm=f28ee0c0f617f4805cc55759c7bc182135d714f55c6465fd26e852edddb18a10&"
+        .setDescription("⬆️ อย่าลืมไปโหวตกันด้วยไลฟ์ครั้งถัดไปภาคไร")
+        .addFields(
+          { name: 'ล่าแย้ทุกวัน', value: 'จ. - พฤ.', inline: true },
+          { name: 'เนื้อเรื่องทุกวัน', value: 'ส.', inline: true },
+          { name: 'เวลา', value: '20:30 - 23:00', inline: true },
         )
         .setImage(value2)
-        .setTimestamp()
-        .setFooter({
-          text: "Tiktok Live",
-          iconURL:
-            "https://store-images.s-microsoft.com/image/apps.4784.13634052595610511.c45457c9-b4af-46b0-8e61-8d7c0aec3f56.3d483847-81a6-4078-8f83-a35c5c38ee92?h=464",
-        });
 
       // ส่ง embed กลับไปยังผู้ใช้ที่ส่งข้อความมา
       channel.send({ embeds: [exampleEmbed] });
